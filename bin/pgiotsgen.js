f6dc71098980db520ffe6c36e15b91df268414c8
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-01-05 23:21:57 +0200

    Create generate function, add eslint, etc

--- bin/pgiotsgen.js
+++ bin/pgiotsgen.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-const { docopt } = require("docopt");
+const { docopt } = require('docopt');
+const generate = require('../lib/generate');
 
 const doc = `
 Generates io-ts contracts from postgresql database schema.
 
 Usage:
-  pgiotsgen --host=<host> [--port=<port>] --username=<username> --password=<password> --outdir=<outdir> [--index]
+  pgiotsgen --host=<host> [--port=<port>] --username=<username> --password=<password> --database=<database> --outdir=<outdir> [--index]
   pgiotsgen --help
 
 Options:
@@ -14,15 +15,28 @@ Options:
   -p --port=<port>            Specifies the TCP port on which the server is listening for connections.
   -u --username=<username>    User name to connect as.
   -w --password=<password>    Password used to connect.
+  -d --database=<database>    Database name to connect to.
   -o --outdir=<outdir>        Directory to generate contracts to.
   -i --index                  Also generate root index.ts file for generated contracts.
 `;
 
 const {
-  "--host": hostname,
-  "--port": port,
-  "--username": username,
-  "--password": password,
-  "--outdir": outDir,
-  "--index": createIndex
+  '--host': hostname,
+  '--port': port,
+  '--username': username,
+  '--password': password,
+  '--database': database,
+  '--outdir': outDir,
+  '--index': createIndexFile,
 } = docopt(doc);
+
+generate(hostname, port, username, password, database, outDir, createIndexFile).then(
+  () => {
+    process.exit(0);
+  },
+  err => {
+    // eslint-disable-next-line no-console
+    console.error(err);
+    process.exit(1);
+  },
+);
